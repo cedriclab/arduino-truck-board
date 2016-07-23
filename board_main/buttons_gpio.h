e211bd65894b8bc77d2d9ec2d8abeb60f0ecce3e
cedriclab <cedric.levlab@gmail.com>
2016-07-23 13:17:13 -0400

    Updated data format to fit single-byte model

--- board_main/buttons_gpio.h
+++ board_main/buttons_gpio.h
@@ -1,10 +1,10 @@
-const int MAX_BUTTON_COUNT = 8; //Max number of buttons = 8, because button states are stored over one byte
+const uint8_t MAX_BUTTON_COUNT = 8; //Max number of buttons = 8, because button states are stored over one byte
 
-unsigned char button_states = 0x00; //all-zeros byte
-int button_count = 4; // Default value
-int first_pin = 0; //Default value
+uint8_t button_states = 0x00; //all-zeros byte
+uint8_t button_count = 4; // Default value
+uint8_t first_pin = 0; //Default value
 
-void buttons_gpio_update_count(int new_count) {
+void buttons_gpio_update_count(uint8_t new_count) {
 	if (new_count > MAX_BUTTON_COUNT) {
 		Serial.println("Maximum button count is 8");
 		return;
@@ -12,24 +12,28 @@ void buttons_gpio_update_count(int new_count) {
 	button_count = new_count;
 }
 
-void buttons_gpio_set_first_pin(int pin_number) { //Assumes that contiguous pins will be used
+void buttons_gpio_set_first_pin(uint8_t pin_number) { //Assumes that contiguous pins will be used
 	first_pin = pin_number;
 }
 
 void buttons_gpio_update_pin_states() {
-	int i;
+	uint8_t i;
 	for (i=0; i<button_count; i++) {
 		digitalWrite(first_pin+i, ((button_states >> i) & 1) ? HIGH : LOW);
 	}
 }
 
-void buttons_gpio_update_button_states(unsigned char new_button_states) {
+uint8_t buttons_gpio_get_button_states() {
+	return button_states;
+}
+
+void buttons_gpio_update_button_states(uint8_t new_button_states) {
 	button_states = new_button_states;
 	buttons_gpio_update_pin_states();
 }
 
 void buttons_gpio_pins_setup() {
-	int i;
+	uint8_t i;
 	for (i=0; i<button_count; i++) {
 		pinMode(first_pin+i, OUTPUT);
 		digitalWrite(first_pin+i, LOW);
